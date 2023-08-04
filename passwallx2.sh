3a0c58ba725dfe78234838fb763ce473cbf33516
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-08-04 13:40:30 +0330

    Create passwallx2.sh

---
+++ passwallx2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+NC='\033[0m' # No Color
+
+
+
+echo "Running as root..."
+sleep 2
+clear
+
+### Install package ###
+
+opkg update
+
+opkg install luci-app-passwall2
+
+echo -e "${GREEN}Done ! ${NC}"
