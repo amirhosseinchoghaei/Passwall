d3e29326e2bdfff9e96242b02e1068516496d0b8
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2023-08-04 13:37:02 +0330

    Create passwallx.sh

---
+++ passwallx.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+NC='\033[0m' # No Color
+
+
+
+echo "Running as root..."
+sleep 2
+clear
+
+### Update Packages ###
+
+opkg update
+
+### Add Src ###
+
+wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
+
+opkg-key add passwall.pub
+
+
+read release arch << EOF
+$(. /etc/openwrt_release ; echo ${DISTRIB_RELEASE%.*} $DISTRIB_ARCH)
+EOF
+for feed in passwall_luci passwall_packages passwall2; do
+  echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> /etc/opkg/customfeeds.conf
+done
+
+### Install package ###
+
+opkg update
+
+opkg install luci-app-passwall
+
+opkg remove dnsmasq
+
+opkg install ipset
+sleep 2
+opkg install ipt2socks
+sleep 2
+opkg install iptables
+sleep 2
+opkg install iptables-legacy
+sleep 2
+opkg install iptables-mod-conntrack-extra
+sleep 2
+opkg install iptables-mod-iprange
+sleep 2
+opkg install iptables-mod-socket
+sleep 2
+opkg install iptables-mod-tproxy
+sleep 2
+opkg install kmod-ipt-nat
+sleep 2
+opkg install dnsmasq-full
+sleep 2
+
+echo -e "${GREEN}Done ! ${NC}"
