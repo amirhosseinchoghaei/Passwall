480b8768acc4890853e80e875b6f40a28d435dd6
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2024-02-27 10:23:17 +0330

    Update passwall2x.sh

--- passwall2x.sh
+++ passwall2x.sh
@@ -14,9 +14,15 @@ clear
 
 uci set system.@system[0].zonename='Asia/Tehran'
 
+uci set network.wan.dns='1.1.1.1'
+
 uci set system.@system[0].timezone='<+0330>-3:30'
 
 uci commit system
+
+uci commit network
+
+uci commit
 
 /sbin/reload_config
 
@@ -138,6 +144,7 @@ uci set passwall2.@global_forwarding[0].tcp_no_redir_ports='disable'
 uci set passwall2.@global_forwarding[0].udp_no_redir_ports='disable'
 uci set passwall2.@global_forwarding[0].tcp_redir_ports='1:65535'
 uci set passwall2.@global_forwarding[0].udp_redir_ports='1:65535'
+uci set passwall2.@global[0].remote_dns='8.8.4.4'
 
 uci set passwall2.Direct=shunt_rules
 uci set passwall2.Direct.network='tcp,udp'
