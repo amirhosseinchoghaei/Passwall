c8f544ad55db68fd471c9813c48e11a324f43664
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2024-03-04 17:09:07 +0330

    Update passwallx.sh

--- passwallx.sh
+++ passwallx.sh
@@ -257,6 +257,7 @@ uci set passwall.@global[0].remote_dns='8.8.4.4'
 uci set passwall.@global[0].dns_mode='udp'
 uci set passwall.@global[0].udp_node='tcp'
 uci set passwall.@global[0].remote_dns='8.8.4.4'
+uci set passwall.@global[0].chn_list='0'
 
 uci commit passwall
 
