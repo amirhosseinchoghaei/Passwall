72a7bbd79496ca2e3d67911d03083be4b69d3c97
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2024-04-04 23:14:10 +0330

    Update passwall2x.sh

--- passwall2x.sh
+++ passwall2x.sh
@@ -32,6 +32,22 @@ uci commit
 
 /sbin/reload_config
 
+SNNAP=`grep -o SNAPSHOT /etc/openwrt_release | sed -n '1p'`
+
+if [ "$SNNAP" == "SNAPSHOT" ]; then
+
+echo -e "${YELLOW} SNAPSHOT Version Detected ! ${NC}"
+
+rm -f passwalls.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwalls.sh && chmod 777 passwalls.sh && sh passwalls.sh
+
+exit 1
+
+ else
+           
+echo -e "${GREEN} Updating Packages ... ${NC}"
+
+fi
+
 ### Update Packages ###
 
 opkg update
