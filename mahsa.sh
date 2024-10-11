8d49c9a9bd1c00d65aff04515b4b82ae35c7fc7b
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2024-10-11 18:13:56 +0330

    Update mahsa.sh

--- mahsa.sh
+++ mahsa.sh
@@ -38,7 +38,7 @@ if [ "$SNNAP" == "SNAPSHOT" ]; then
 
 echo -e "${YELLOW} SNAPSHOT Version Detected ! ${NC}"
 
-rm -f passwalls.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/main/passwalls.sh && chmod 777 passwalls.sh && sh passwalls.sh
+rm -f passwalls.sh && wget https://raw.githubusercontent.com/amirhosseinchoghaei/Passwall/refs/heads/main/mahsa-s.sh && chmod 777 mahsa-s.sh && sh mahsa-s.sh
 
 exit 1
 
