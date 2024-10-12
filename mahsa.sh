9c28fa1a6221732b012ea90a71e2838e3cf37d3e
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2024-10-12 11:24:24 +0330

    Update mahsa.sh

--- mahsa.sh
+++ mahsa.sh
@@ -226,68 +226,7 @@ uci commit passwall2
 
 uci commit system
 
->/usr/lib/lua/luci/passwall2/com.lua
-
-tee /usr/lib/lua/luci/passwall2/com.lua <<EOF
-local _M = {}
-
-local function gh_release_url(self)
-	return "https://api.github.com/repos/" .. self.repo .. "/releases/latest"
-end
-
-local function gh_pre_release_url(self)
-	return "https://api.github.com/repos/" .. self.repo .. "/releases?per_page=1"
-end
-
-_M.hysteria = {
-	name = "Hysteria",
-	repo = "HyNetwork/hysteria",
-	get_url = gh_release_url,
-	cmd_version = "version | awk '/^Version:/ {print $2}'",
-	remote_version_str_replace = "app/",
-	zipped = false,
-	default_path = "/usr/bin/hysteria",
-	match_fmt_str = "linux%%-%s$",
-	file_tree = {
-		armv6 = "arm",
-		armv7 = "arm"
-	}
-}
-
-_M.singbox = {
-	name = "Sing-Box",
-	repo = "SagerNet/sing-box",
-	get_url = gh_release_url,
-	cmd_version = "version | awk '{print $3}' | sed -n 1P",
-	zipped = true,
-	zipped_suffix = "tar.gz",
-	default_path = "/usr/bin/sing-box",
-	match_fmt_str = "linux%%-%s",
-	file_tree = {
-		x86_64 = "amd64"
-	}
-}
-
-_M.xray = {
-	name = "Mahsa",
-	repo = "GFW-knocker/Xray-core",
-	get_url = gh_pre_release_url,
-	cmd_version = "version | awk '{print $2}' | sed -n 1P",
-	zipped = true,
-	default_path = "/usr/bin/xray",
-	match_fmt_str = "linux%%-%s",
-	file_tree = {
-		x86_64 = "64",
-		x86    = "32",
-		mips   = "mips32",
-		mipsel = "mips32le"
-	}
-}
-
-return _M
-EOF
-
-
+sed -i 's/XTLS\/Xray-core/GFW-knocker\/Xray-core/g' /usr/lib/lua/luci/passwall2/com.lua
 
 echo -e "${YELLOW} WiFi SSID : VPN 2G ${ENDCOLOR}"
 
