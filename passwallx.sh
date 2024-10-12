87862475737db7e6123bae5e8e50c92ea42e4838
AmirHosseinChoghaei <127476407+amirhosseinchoghaei@users.noreply.github.com>
2024-10-12 12:53:05 +0330

    Update passwallx.sh

--- passwallx.sh
+++ passwallx.sh
@@ -14,19 +14,7 @@ clear
 
 uci set system.@system[0].zonename='Asia/Tehran'
 
-uci set network.wan.peerdns="0"
-
-uci set network.wan6.peerdns="0"
-
-uci set network.wan.dns='1.1.1.1'
-
-uci set network.wan6.dns='2001:4860:4860::8888'
-
 uci set system.@system[0].timezone='<+0330>-3:30'
-
-uci commit system
-
-uci commit network
 
 uci commit
 
